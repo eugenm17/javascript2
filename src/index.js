6b31c1ce08e511dc5e368ec3a285ef2db2dc680d
eugenm17 <85393003+eugenm17@users.noreply.github.com>
2021-10-02 13:57:25 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+let colors = ["green", "blue", "yellow"];
+
+for (let i = 0; i < colors.length; i++) {
+for (let colors of colors){
+  console.log(color);
+console.log(colors [i]);
+
+}
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors.length);
+colors.push("pink");
+console.log(colors[3]);
